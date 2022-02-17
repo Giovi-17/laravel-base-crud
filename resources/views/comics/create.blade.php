008caad0c2874ef251791b2c6dc7795d19037a0e
Giovi-17 <giovanni.nuzzo17@gmail.com>
2022-02-17 11:32:22 +0100

    fix name submit

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -24,42 +24,50 @@
             <div>
 
                 <label for="thumb" class="form-label">Thumb</label>
-                <input type="text" class="form-control" id="thumb" name="title">
+                <input type="text" class="form-control" id="thumb" name="thumb">
 
             </div>
 
             <div>
 
                 <label for="price" class="form-label">Price</label>
-                <input type="text" class="form-control" id="price" name="title">
+                <input type="text" class="form-control" id="price" name="price">
 
             </div>
 
             <div>
 
                 <label for="series" class="form-label">Series</label>
-                <input type="text" class="form-control" id="series" name="title">
+                <input type="text" class="form-control" id="series" name="series">
 
             </div>
 
             <div>
 
                 <label for="sale_data" class="form-label">Sale_data</label>
-                <input type="text" class="form-control" id="sale_data" name="title">
+                <input type="text" class="form-control" id="sale_data" name="sale_data">
 
             </div>
 
             <div>
 
                 <label for="type" class="form-label">Type</label>
-                <input type="text" class="form-control" id="type" name="title">
+                <input type="text" class="form-control" id="type" name="type">
 
             </div>
 
             <div class="form-group">
-                <label for="exampleFormControlTextarea1">Description</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-              </div>
+                
+                <label for="description">Description</label>
+                <textarea class="form-control" id="description" name="description" rows="3"></textarea>
+            
+            </div>
+
+            <div>
+
+                <button type="submit" class="btn btn-primary mb-2">Submit</button>
+
+            </div>
     
         </form>
 
